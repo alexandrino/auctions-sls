0a941148f1f1cbd5e80c9d9b69f38976796ecc38
Alexandrino Santana <alexandrino.santana@gmail.com>
2021-08-31 14:48:51 -0300

    Send closed auctions to the mailQueue

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -2,14 +2,19 @@ import AWS from 'aws-sdk'
 import logger from './logger'
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const sqs = new AWS.SQS()
 
 async function closeAuction(auction) {
   const { id } = auction
+  const {
+    AUCTIONS_TABLE_NAME: tableName,
+    MAIL_QUEUE_URL: mailQueueUrl,
+  } = process.env
   logger.info(`closeAuction.start ${id}`)
 
   try {
     const params = {
-      TableName: process.env.AUCTIONS_TABLE_NAME,
+      TableName: tableName,
       Key: {
         id,
       },
@@ -21,12 +26,24 @@ async function closeAuction(auction) {
         '#status': 'status',
       },
     }
-    const result = await dynamoDb
+    await dynamoDb
       .update(params)
       .promise()
 
+    const notifySeller = sqs
+      .sendMessage({
+        QueueUrl: mailQueueUrl,
+        MessageBody: JSON.stringify({
+          subject: `auction closed ${id}`,
+        }),
+      })
+      .promise()
+
+    // notifyBidder
+    const result = await Promise.all([notifySeller])
     logger.info(`closeAuction.success ${id}`)
-    return result.Items
+
+    return result
   } catch (error) {
     logger.error('closeAuction.error', {
       errorMessage: error.message,
